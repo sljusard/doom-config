a9581fc03db6a2fba96297d41bcd8c947590c3a9
Denis Sliusar <sljusarde@gmail.com>
2026-07-05 23:40:47 +0300

    feat: removed elfeed-goodies

--- packages.el
+++ packages.el
@@ -10,6 +10,7 @@
 ;; Use 'C-h f package\!' to look up documentation for the `package!' macro.
 
 (package! reverse-im)
+(package! elfeed-goodies :disable t)
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;; (package! some-package)
